37a0142fadfb37ca6e91a81d23bc4542845fad59
JuneKei <85489867+JuneKei@users.noreply.github.com>
2022-05-19 20:50:36 +0900

    Create ex04.cpp
    변수, 자료형

---
+++ InputOutput/InputOutput/ex04.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+
+int main() {
+	//byte : 컴퓨터에서 데이터를 처리하는 가장 작은 단위
+	bool b; // 1byte 0,1이여도 처리단위가 1byte라 1byte
+
+	char c; // 1byte
+	short s; // 2byte
+	//정수형
+	int i; //32bit = 4byte 시스템에 따라서 자동 결정이지만, 대체로 long형태 -2^31~2^31-1
+	long l; // 4byte
+	long long ll; // 8byte
+
+	//실수형
+	float f; // 4byte
+	double d; // 8byte
+
+	//
+	unsigned int ui; //2^32 -1 
+	
+	/*
+	void : 리턴 값이 없는 함수의 자료형
+	*/
+	
+}
